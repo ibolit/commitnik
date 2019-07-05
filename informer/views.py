d0a6de3fa0451d882f2cf3023baec92bf63b04db
Timofey <dtv@me.com>
2019-07-05 09:21:11 +0300

    Bring in DRF

--- informer/views.py
+++ informer/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class InformerView(View):
+class InformerView(APIView):
     def get(self, request):
-        return HttpResponse('Hello, dude')
+        return Response({'commit': '000000'})
